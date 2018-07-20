882df7d8871272f24dd2f62642ef36e708baf78c
Anish Duwal <duwalanise@gmail.com>
2018-07-20 08:43:35 +0545

    pushed all

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import App from './NewCode';
 import registerServiceWorker from './registerServiceWorker';
 
 initializeIcons();
